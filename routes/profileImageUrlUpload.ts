5463d340d65a8edc261ebbca84a5960b4edbac97
Saad Khalid <91637722+stargazer4@users.noreply.github.com>
2023-04-10 06:10:14 +0000

    testing copilot chat SSRF fix

--- routes/profileImageUrlUpload.ts
+++ routes/profileImageUrlUpload.ts
@@ -1,42 +1,83 @@
-/*
- * Copyright (c) 2014-2022 Bjoern Kimminich & the OWASP Juice Shop contributors.
- * SPDX-License-Identifier: MIT
- */
+import fs from 'fs';
+import { Request, Response, NextFunction } from 'express';
+import { UserModel } from '../models/user';
+import { getErrorMessage } from '../lib/utils';
+import { authenticatedUsers } from '../lib/insecurity';
+import request from 'request';
+import logger from '../lib/logger';
+import url from 'url';
 
-import fs = require('fs')
-import { Request, Response, NextFunction } from 'express'
-
-import { UserModel } from '../models/user'
-const utils = require('../lib/utils')
-const security = require('../lib/insecurity')
-const request = require('request')
-const logger = require('../lib/logger')
-
-module.exports = function profileImageUrlUpload () {
-  return (req: Request, res: Response, next: NextFunction) => {
-    if (req.body.imageUrl !== undefined) {
-      const url = req.body.imageUrl
-      if (url.match(/(.)*solve\/challenges\/server-side(.)*/) !== null) req.app.locals.abused_ssrf_bug = true
-      const loggedInUser = security.authenticatedUsers.get(req.cookies.token)
-      if (loggedInUser) {
-        const imageRequest = request
-          .get(url)
-          .on('error', function (err: unknown) {
-            UserModel.findByPk(loggedInUser.data.id).then(async (user: UserModel | null) => { return await user?.update({ profileImage: url }) }).catch((error: Error) => { next(error) })
-            logger.warn(`Error retrieving user profile image: ${utils.getErrorMessage(err)}; using image link directly`)
-          })
-          .on('response', function (res: Response) {
-            if (res.statusCode === 200) {
-              const ext = ['jpg', 'jpeg', 'png', 'svg', 'gif'].includes(url.split('.').slice(-1)[0].toLowerCase()) ? url.split('.').slice(-1)[0].toLowerCase() : 'jpg'
-              imageRequest.pipe(fs.createWriteStream(`frontend/dist/frontend/assets/public/images/uploads/${loggedInUser.data.id}.${ext}`))
-              UserModel.findByPk(loggedInUser.data.id).then(async (user: UserModel | null) => { return await user?.update({ profileImage: `/assets/public/images/uploads/${loggedInUser.data.id}.${ext}` }) }).catch((error: Error) => { next(error) })
-            } else UserModel.findByPk(loggedInUser.data.id).then(async (user: UserModel | null) => { return await user?.update({ profileImage: url }) }).catch((error: Error) => { next(error) })
-          })
-      } else {
-        next(new Error('Blocked illegal activity by ' + req.connection.remoteAddress))
+export default function profileImageUrlUpload() {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const imageUrl = req.body.imageUrl;
+      if (!imageUrl) {
+        throw new Error('Missing image URL');
       }
+
+      const parsedUrl = url.parse(imageUrl);
+      const hostname = parsedUrl.hostname;
+      const pathname = parsedUrl.pathname;
+
+      const allowedHosts = ['example.com', 'trustedhost.com'];
+      if (!allowedHosts.includes(hostname)) {
+        throw new Error('Invalid image URL');
+      }
+
+      if (pathname.includes('..')) {
+        throw new Error('Invalid image URL');
+      }
+
+      if (pathname.match(/(.)*solve\/challenges\/server-side(.)*/) !== null) {
+        req.app.locals.abused_ssrf_bug = true;
+      }
+
+      const loggedInUser = authenticatedUsers.get(req.cookies.token);
+      if (!loggedInUser) {
+        throw new Error('Unauthorized');
+      }
+
+      const imageRequest = request.get(imageUrl);
+      const ext = ['jpg', 'jpeg', 'png', 'svg', 'gif'].includes(parsedUrl.pathname.split('.').slice(-1)[0].toLowerCase()) ? parsedUrl.pathname.split('.').slice(-1)[0].toLowerCase() : 'jpg';
+      const imagePath = `frontend/dist/frontend/assets/public/images/uploads/${loggedInUser.data.id}.${ext}`;
+      const imageUrlPath = `/assets/public/images/uploads/${loggedInUser.data.id}.${ext}`;
+
+      imageRequest.on('error', (err: Error) => {
+        logger.warn(`Error retrieving user profile image: ${getErrorMessage(err)}; using image link directly`);
+        UserModel.findByPk(loggedInUser.data.id).then(async (user: UserModel | null) => {
+          if (user) {
+            await user.update({ profileImage: imageUrl });
+          }
+        }).catch((error: Error) => {
+          next(error);
+        });
+      });
+
+      imageRequest.on('response', (response) => {
+        if (response.statusCode === 200) {
+          imageRequest.pipe(fs.createWriteStream(imagePath));
+          UserModel.findByPk(loggedInUser.data.id).then(async (user: UserModel | null) => {
+            if (user) {
+              await user.update({ profileImage: imageUrlPath });
+            }
+          }).catch((error: Error) => {
+            next(error);
+          });
+        } else {
+          UserModel.findByPk(loggedInUser.data.id).then(async (user: UserModel | null) => {
+            if (user) {
+              await user.update({ profileImage: imageUrl });
+            }
+          }).catch((error: Error) => {
+            next(error);
+          });
+        }
+      });
+
+      res.location(process.env.BASE_PATH + '/profile');
+      res.redirect(process.env.BASE_PATH + '/profile');
+    } catch (error) {
+      next(error);
     }
-    res.location(process.env.BASE_PATH + '/profile')
-    res.redirect(process.env.BASE_PATH + '/profile')
-  }
+  };
 }
